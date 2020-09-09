bd059039f3d64cb1a763c09db76bfa98380206fe
Kosenko <Kosenko.2000@yandex.ru>
2020-09-09 22:15:17 +0500

    right version

--- src/ru/geekbrains/homeworks/Main.java
+++ src/ru/geekbrains/homeworks/Main.java
@@ -9,53 +9,51 @@ public class Main {
     public static void main(String[] args) {
 
         float[] arr = new float[size];
+        System.out.println("Время работы первого метода: " + doTheFirstMethod(arr));
+        doTheSecondMethod(arr);
+    }
+    public static long doTheFirstMethod(float[] arr){
 
         for (int i = 0; i<arr.length; i++){
             arr[i] = 1;
         }
-
         long time0 = System.currentTimeMillis();
 
-        System.out.println("Время работы первого метода: " + doTheFirstMethod(arr, time0));
-        doTheSecondMethod(arr,doTheFirstMethod(arr, time0),time0);
-
-
-
-    }
-    public static long doTheFirstMethod(float[] arr, long time0){
         calculate(arr);
         long time1 = System.currentTimeMillis();
         return (time1 - time0);
     }
-    public static void doTheSecondMethod(float[] arr, long time1, long time0) {
+    public static void doTheSecondMethod(float[] arr) {
+        for (int i = 0; i<arr.length; i++){
+            arr[i] = 1;
+        }
+        long time0 = System.currentTimeMillis();
 
-         Object lock1 = new Object();
         float[] arrPart1 = new float[size];
         float[] arrPart2 = new float[size];
         System.arraycopy(arr, 0, arrPart1, 0, size/2);
         System.arraycopy(arr, size/2, arrPart2,size/2, size/2);
 
-
             Thread MyThreadTheFirst = new Thread() {
                 @Override
                 public void run() {
-                    super.run();
-                    calculate(arrPart1);
-                }
-            };
-
-
-
-            Thread MyThreadTheSecond = new Thread() {
-                @Override
-                public void run() {
-                    super.run();
-                    synchronized (lock1) {
-                        calculate(arrPart2);
+                    synchronized (arrPart1) {
+                        super.run();
+                        calculate(arrPart1);
                     }
                 }
             };
 
+            Thread MyThreadTheSecond = new Thread() {
+
+                @Override
+                public void run() {
+                    synchronized (arrPart2) {
+                        super.run();
+                        calculate(arrPart2);
+                    }
+                }
+            };
 
         MyThreadTheFirst.start();
         MyThreadTheSecond.start();
@@ -63,10 +61,12 @@ public class Main {
         System.arraycopy(arrPart1, 0, arr, 0, size/2);
         System.arraycopy(arrPart2, 0, arr, size/2, size/2);
 
-        long time2 = System.currentTimeMillis();
-
-        System.out.println("Время работы второго метода: " + (time2-time1-time0));
-
+        synchronized (arrPart1) {
+            synchronized (arrPart2) {
+                long time2 = System.currentTimeMillis();
+                System.out.println("Время работы второго метода: " + (time2 - time0));
+            }
+        }
 
     }
     private static float[] calculate (float[]arr) {
@@ -75,8 +75,6 @@ public class Main {
         }
         return arr;
     }
-
-
     }
 
 
